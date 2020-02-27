9ce4a0a262ed3c4f3963d810244a3654fd5de9a1
yqiu23 <60491664+yqiu23@users.noreply.github.com>
2020-02-27 15:41:59 -0600

    Update HW3.c

--- HW3.c
+++ HW3.c
@@ -5,83 +5,70 @@
 #include <sys/wait.h>
 #include <signal.h>
 
-int builtin_command(char **argv)
+
+#define MAXARGS 128
+#define MAXLINE 500
+
+/* Function prototypes */
+void eval(char *cmdline);
+int parseline(char *buf, char **argv);
+int builtin_command(char **argv);
+void sigint_handler(int sig);
+void sigtstp_handler(int sig);
+
+ int main()
  {
-    if (!strcmp(argv[0], "quit"))
-       exit(0);
-    if (!strcmp(argv[0], "&")) 
-       return 1;
-    return 0; 
- } 
+    //signal(SIGINT, sigint_handler);
+    //signal(SIGTSTP, sigtstp_handler);
+    char cmdline[MAXLINE]; /* Command line */
 
-void sigint_handler(int sig){
-  char msg[] = "sigint\n";
-  char msg2[] = "CS361 >";
-  write(1, msg, sizeof(msg));
-  write(1, msg2, sizeof(msg2));
-}
+     while (1) {
+        /* Read */
+	signal(SIGINT, sigint_handler);
+        signal(SIGTSTP, sigtstp_handler);
+        printf("CS361 >");
+        fgets(cmdline, MAXLINE, stdin);
+	//signal(SIGINT, sigint_handler);
+    	//signal(SIGTSTP, sigtstp_handler);
+        if (feof(stdin))
+           exit(0);
+        /* Evaluate & Forking */
+	int pid = fork();
+	if (pid == 0) {
+            eval(cmdline);
+   	}
+	else { 
+	    int status;
+            wait(&status);
+            printf("pid:%d status:%d\n", pid, WEXITSTATUS(status));
+	}
+     }
 
-void sigtstp_handler(int sig) {
-  char msg[] = "sigtstp\n";
-  char msg2[] = "CS361 >";
-  write(1, msg, sizeof(msg));
-  write(1, msg2, sizeof(msg2));
-}
-
- int parseline(char *buf, char **argv)
+ }
+/* eval - Evaluate a command line */
+ void eval(char *cmdline)
  {
-    char *delim; 
-    int argc; 
-    int bg; 
-
-    buf[strlen(buf)-1] = ' '; 
-    while (*buf && (*buf == ' ')) 
-    buf++;
-
-
-    argc = 0;
-	while ((delim = strchr(buf, ' '))) {
-	   argv[argc++] = buf;
-	   *delim = '\0';
-	    buf = delim + 1;
-	    while (*buf && (*buf == ' ')) 
-	       buf++;
-}
-    argv[argc] = NULL;
-
-    if (argc == 0) 
-       return 1;
-
-
-    if ((bg = (*argv[argc-1] == '&')) != 0)
-       argv[--argc] = NULL;
-
-    return bg;
-}
-
-void Eval(char *cmdline)
- {
-	 char *argv[128]; 
-	 char buf[500]; 
-	 int bg; 
-	 pid_t pid; 
+	 char *argv[MAXARGS]; /* Argument list execve() */
+	 char buf[MAXLINE]; /* Holds modified command line */
+	 int bg; /* Should the job run in bg or fg? */
+	 pid_t pid; /* Process id */
 
 	 strcpy(buf, cmdline);
 	 bg = parseline(buf, argv);
 	 if (argv[0] == NULL)
-	 return; 
-
+	 return; /* Ignore empty lines */
+	 /* EXIT */
 	 if (strcmp(argv[0], "exit") == 0)
 		 exit(0);
 	 if (!builtin_command(argv)) {
-	 if ((pid = fork()) == 0) { 
+	 if ((pid = fork()) == 0) { /* Child runs user job */
 	 if (execve(argv[0], argv, __environ) < 0) {
 	 	printf("%s: Command not found.\n", argv[0]);
 	 	exit(0);
 	 	}
 	 }
 
-
+ /* Parent waits for foreground job to terminate */
 	 if (!bg) {
 	    int status;
 	 if (waitpid(pid, &status, 0) < 0)
@@ -92,33 +79,60 @@ void Eval(char *cmdline)
 	 }
 	 return;
  }
- 
 
-int main()
-{
-    char cmdline[500];
-    
-    while(1){
-        signal(SIGINT, sigint_handler);
-        signal(SIGINT, sigtstp_handler);
-        printf("CS361 >");
-        fgets(cmdline, 500, stdin);
-        
-        if(feof(stdin)){
-            exit(0);
-        }
-        
-        int pid = fork();
-        if(pid == 0){
-            Eval(cmdline);
-        }
-        else{
-            int stat;
-            wait(&stat);
-            printf("pid:%d status:%d\n", pid, WEXITSTATUS(stat));
-        }
-        
-    }
+ /* If first arg is a builtin command, run it and return true */
+ int builtin_command(char **argv)
+ {
+    if (!strcmp(argv[0], "quit")) /* quit command */
+       exit(0);
+    if (!strcmp(argv[0], "&")) /* Ignore singleton & */
+       return 1;
+    return 0; /* Not a builtin command */
+ }
 
-    return 0;
+ /* parseline - Parse the command line and build the argv array */
+ int parseline(char *buf, char **argv)
+ {
+    char *delim; /* Points to first space delimiter */
+    int argc; /* Number of args */
+    int bg; /* Background job? */
+
+    buf[strlen(buf)-1] = ' '; /* Replace trailing â€™\nâ€™ with space */
+    while (*buf && (*buf == ' ')) /* Ignore leading spaces */
+    buf++;
+
+    /* Build the argv list */
+    argc = 0;
+	while ((delim = strchr(buf, ' '))) {
+	   argv[argc++] = buf;
+	   *delim = '\0';
+	    buf = delim + 1;
+	    while (*buf && (*buf == ' ')) /* Ignore spaces */
+	       buf++;
+}
+    argv[argc] = NULL;
+
+    if (argc == 0) /* Ignore blank line */
+       return 1;
+
+    /* Should the job run in the background? */
+    if ((bg = (*argv[argc-1] == '&')) != 0)
+       argv[--argc] = NULL;
+
+    return bg;
+}
+/* ^c */
+void sigint_handler(int sig){
+  char msg[] = "\ncaught sigint\n";
+  char msg2[] = "CS361 >";
+  write(1, msg, sizeof(msg));
+  write(1, msg2, sizeof(msg2));
+}
+
+/* ^z */
+void sigtstp_handler(int sig) {
+  char msg[] = "\ncaught sigtstp\n";
+  char msg2[] = "CS361 >";
+  write(1, msg, sizeof(msg));
+  write(1, msg2, sizeof(msg2));
 }
