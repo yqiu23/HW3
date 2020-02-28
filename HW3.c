e492f281a412c15aa7f2cc3e9c5e51d1a4828ade
yqiu23 <60491664+yqiu23@users.noreply.github.com>
2020-02-27 21:40:21 -0600

    Update HW3.c

--- HW3.c
+++ HW3.c
@@ -9,29 +9,33 @@
 #define MAXARGS 128
 #define MAXLINE 500
 
-void Eval(char *cmd);
-int parse(char *buf, char **argv);
+/* Function prototypes */
+void eval(char *cmdline);
+int parseline(char *buf, char **argv);
 int builtin_command(char **argv);
 void sigint_handler(int sig);
 void sigtstp_handler(int sig);
 
  int main()
  {
-    char cmd[MAXLINE]; 
+    //signal(SIGINT, sigint_handler);
+    //signal(SIGTSTP, sigtstp_handler);
+    char cmdline[MAXLINE]; /* Command line */
 
      while (1) {
-
+        /* Read */
 	signal(SIGINT, sigint_handler);
         signal(SIGTSTP, sigtstp_handler);
         printf("CS361 >");
-        fgets(cmd, MAXLINE, stdin);
-
+        fgets(cmdline, MAXLINE, stdin);
+	//signal(SIGINT, sigint_handler);
+    	//signal(SIGTSTP, sigtstp_handler);
         if (feof(stdin))
            exit(0);
-           
+        /* Evaluate & Forking */
 	int pid = fork();
 	if (pid == 0) {
-            Eval(cmd);
+            eval(cmdline);
    	}
 	else { 
 	    int status;
@@ -41,82 +45,83 @@ void sigtstp_handler(int sig);
      }
 
  }
-
- void Eval(char *cmd)
+/* eval - Evaluate a command line */
+ void eval(char *cmdline)
  {
-	 char *argv[MAXARGS]; 
-	 char buf[MAXLINE];
-	 int bg; 
-	 pid_t pid; 
+	 char *argv[MAXARGS]; /* Argument list execve() */
+	 char buf[MAXLINE]; /* Holds modified command line */
+	 int bg; /* Should the job run in bg or fg? */
+	 pid_t pid; /* Process id */
 
-	 strcpy(buf, cmd);
-	 bg = parse(buf, argv);
+	 strcpy(buf, cmdline);
+	 bg = parseline(buf, argv);
 	 if (argv[0] == NULL)
-	 return; 
-
+	 return; /* Ignore empty lines */
+	 /* EXIT */
 	 if (strcmp(argv[0], "exit") == 0)
 		 exit(0);
 	 if (!builtin_command(argv)) {
-	 if ((pid = fork()) == 0) { 
+	 if ((pid = fork()) == 0) { /* Child runs user job */
 	 if (execve(argv[0], argv, __environ) < 0) {
 	 	printf("%s: Command not found.\n", argv[0]);
 	 	exit(0);
 	 	}
 	 }
 
-
+ /* Parent waits for foreground job to terminate */
 	 if (!bg) {
 	    int status;
 	 if (waitpid(pid, &status, 0) < 0)
 	    printf("waitfg: waitpid error");
 	 }
 	 else
-	    printf("%d %s", pid, cmd);
+	    printf("%d %s", pid, cmdline);
 	 }
 	 return;
  }
 
-
+ /* If first arg is a builtin command, run it and return true */
  int builtin_command(char **argv)
  {
-    if (!strcmp(argv[0], "quit")) 
+    if (!strcmp(argv[0], "quit")) /* quit command */
        exit(0);
-    if (!strcmp(argv[0], "&")) 
+    if (!strcmp(argv[0], "&")) /* Ignore singleton & */
        return 1;
-    return 0; 
+    return 0; /* Not a builtin command */
  }
 
-
- int parse(char *buf, char **argv)
+ /* parseline - Parse the command line and build the argv array */
+ int parseline(char *buf, char **argv)
  {
-    char *delim; 
-    int argc;
-    int bg; 
+    char *delim; /* Points to first space delimiter */
+    int argc; /* Number of args */
+    int bg; /* Background job? */
 
-    buf[strlen(buf)-1] = ' '; 
-    while (*buf && (*buf == ' ')) 
+    buf[strlen(buf)-1] = ' '; /* Replace trailing â€™\nâ€™ with space */
+    while (*buf && (*buf == ' ')) /* Ignore leading spaces */
     buf++;
 
-
+    /* Build the argv list */
     argc = 0;
 	while ((delim = strchr(buf, ' '))) {
 	   argv[argc++] = buf;
 	   *delim = '\0';
 	    buf = delim + 1;
-	    while (*buf && (*buf == ' ')) 
+	    while (*buf && (*buf == ' ')) /* Ignore spaces */
 	       buf++;
 }
     argv[argc] = NULL;
 
-    if (argc == 0) 
+    if (argc == 0) /* Ignore blank line */
        return 1;
 
+    /* Should the job run in the background? */
     if ((bg = (*argv[argc-1] == '&')) != 0)
        argv[--argc] = NULL;
 
     return bg;
 }
-
+/* ^c */
 void sigint_handler(int sig){
   char msg[] = "\ncaught sigint\n";
   char msg2[] = "CS361 >";
@@ -124,7 +129,7 @@ void sigint_handler(int sig){
   write(1, msg2, sizeof(msg2));
 }
 
-
+/* ^z */
 void sigtstp_handler(int sig) {
   char msg[] = "\ncaught sigtstp\n";
   char msg2[] = "CS361 >";
